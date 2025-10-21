d113ecf2fa508342146e47d7abebf1e20fca67a4
buidinhhuy <daotao@abaii.vn>
2025-10-21 14:44:44 +0700

    Init

--- server/index.ts
+++ server/index.ts
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    serveStatic(app);
+    await setupVite(app, server);
   }
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
